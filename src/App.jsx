8b483591a0c2228dcfd1e51ab6ff2c9723b28fa0
frewcm <frewbefekadu32@gmail.com>
2022-04-10 17:04:30 +0300

    not fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,41 @@ import About from './components/About/About'
 import Portfolio from './components/portfolio/Portfolio';
 import Contact from './components/contact/Contact';
 import './app.scss';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import Mesob from './redirects/Mesob'
+import Movie from './redirects/Movie'
+import Candy from './redirects/Candy'
+import {SliderImage} from './redirects/SliderImage'
+
 
 
 
 
 function App() {
   return (
-    <div className="app">
-    <Nav />
-    <div className='sections'>
-     <Intro />
-     <About />
-     <Portfolio />
-     <Contact />
+    // <div className="app">
+    // <Nav />
+    // <div className='sections'>
+    //  <Intro />
+    //  <About />
+    //  <Portfolio />
+    //  <Contact />
      
-      </div> 
+    //   </div> 
       
-    </div>
+    // </div>
+    <Router>
+      <Nav />
+      <Intro />
+      <About />
+      <Portfolio />
+      <Contact />
+    <Routes>
+      <Route path='/mesob' element={<Mesob slides={SliderImage} />} />
+      <Route path='/movie' element={<Movie />} />
+      <Route path='/candy' element={<Candy />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
-import Mesob from './redirects/Mesob'
-import Movie from './redirects/Movie'
-import Candy from './redirects/Candy'
-import {SliderImage} from './redirects/SliderImage'
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router>
-    <Routes>
-    <Route path='/' element={<App />} />
-    <Route path='/mesob' element={<Mesob slides={SliderImage} />} />
-    <Route path='/movie' element={<Movie />} />
-    <Route path='/candy' element={<Candy />} />
-    </Routes>
-  </Router>
+   <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
