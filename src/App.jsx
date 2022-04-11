4d87d908383c12531eb62d285ec69e19dd961f26
frewcm <frewbefekadu32@gmail.com>
2022-04-11 10:14:05 +0300

    route fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
 import React from "react";
-import Nav from './components/NavBar/Nav';
-import Intro from './components/intro/Intro';
-import About from './components/About/About'
-import Portfolio from './components/portfolio/Portfolio';
-import Contact from './components/contact/Contact';
 import './app.scss';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import Mesob from './redirects/Mesob'
 import Movie from './redirects/Movie'
 import Candy from './redirects/Candy'
 import {SliderImage} from './redirects/SliderImage'
-
+import Collector from './Collector'
 
 
 
 
 function App() {
   return (
-    // <div className="app">
-    // <Nav />
-    // <div className='sections'>
-    //  <Intro />
-    //  <About />
-    //  <Portfolio />
-    //  <Contact />
-     
-    //   </div> 
-      
-    // </div>
     <Router>
-      <Nav />
-      <Intro />
-      <About />
-      <Portfolio />
-      <Contact />
+      
+     
     <Routes>
+      <Route path='/' element={<Collector />} />
       <Route path='/mesob' element={<Mesob slides={SliderImage} />} />
       <Route path='/movie' element={<Movie />} />
       <Route path='/candy' element={<Candy />} />
       </Routes>
+       
     </Router>
+   
   );
 }
 
